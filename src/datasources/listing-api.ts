ccd755cb5eb226ded69bf4f63f27cc7bc41f412d
EmilyEkins <emilyekins@ES-Ekinss-MacBook-Pro.local>
2025-04-08 18:13:12 +0100

    completed course

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,12 +1,25 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
-import { Listing } from "../types";
+import { Listing, Amenity, CreateListingInput } from "../types";
 
 export class ListingAPI extends RESTDataSource {
-    override baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
-    // you'll get a compiler error if you misspell the variable 
-
-    getFeaturedListings(): Promise<Listing[]> {
-        return this.get<Listing[]>("featured-listings");
-    }
-    
+  
+  baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
+  
+  getFeaturedListings(): Promise<Listing[]> {
+    return this.get<Listing[]>("featured-listings");
+  }
+  
+  getListing(listingId: string): Promise<Listing> {
+    return this.get<Listing>(`listings/${listingId}`);
+  }
+  
+  getAmenities(listingId: string): Promise<Amenity[]> {
+    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+  }
+  
+  createListing(listing: CreateListingInput): Promise<Listing> {
+    return this.post<Listing>("listings", {
+      body: { listing }
+    });
+  }
 }
